72510e79eb683e97961e55773da167d1dca06760
rakshithrii <rakshith.16mc@kct.ac.in>
2019-03-16 11:43:52 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+print("Select the operation")
+print("1.Add")
+print("2.subract")
+print("3.multiply")
+print("4.division")
+num1=float(input())
+num2=float(input())
+choise=(input())
+if choise == '1': 
+    print(add(num1,num2)) 
+  
+elif choise == '2': 
+     print(add(num1,num2)) 
+elif choise == '3': 
+    print(add(num1,num2))  
+elif choise == '4': 
+    print(add(num1,num2))
+else: 
+    print("Invalid input") 
+    
+            
+    
+    
+    
+   
